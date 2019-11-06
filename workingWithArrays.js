f2f4177846ae1eb78c00edaf8573643a96052008
Joey Romo <joeyjr95@gmail.com>
2019-11-06 17:13:47 -0500

    stuff

--- workingWithArrays.js
+++ workingWithArrays.js
@@ -4,7 +4,7 @@ function fixBlanks(string){
   let newString = string.split(' ').join('%20');
   return newString;
 }
-console.log(fixBlanks('in the garden'));
+// console.log(fixBlanks('in the garden'));
 
 
 function filterArray(array){
@@ -20,7 +20,7 @@ function filterArray(array){
     return newArray;
   }
 }
-console.log(filterArray([8, 3, 7, 6, 2, 9, 1]));
+const arr = [8, 3, 7, 6];
 
 function filter(arr, filterFunc) {
   const filterArr = []; 
@@ -29,9 +29,68 @@ function filter(arr, filterFunc) {
     if(result) {            
       filterArr.push(arr[i]); 
     }    
-}    
-return filterArr;
+  }    
+  return filterArr;
 }
 
 
-  const oddArr2 = filter(arr, num => num % 2 === 0);
+const lessThanFive = filter(arr, num => num > 4);
+
+// console.log(lessThanFive);
+
+function arrSum(arr){
+  let sum = 0;
+  for(let i=0; i<arr.length; i++) {
+    let newSum = arr[i] + arr[i + 1];
+    if(newSum > sum){
+      sum = newSum;
+    }
+
+  }
+  return sum;
+}
+// console.log(arrSum(arr));
+
+
+let arr1 = [1, 3, 6, 8, 11];
+let arr2 = [2, 3, 5, 8, 9, 10];
+function merge(arr1, arr2){
+  let newArr = arr1.concat(arr2);
+  newArr.sort((a, b) => {  
+    return a-b;
+  });
+  return newArr;
+}
+// console.log(merge(arr1, arr2));
+function remove(string){
+  // const vowels = ('a' || 'e');
+  
+  // let newString = '';
+  // for(let i = 0; i < string.length; i++ ){
+  //   if(string[i] !== vowels){
+  //     newString = newString.concat(string[i]);
+  //   }
+  // }
+  let newString = string.replace(/a/g, '').replace(/e/g, '').replace(/i/g, '').replace(/o/g, '').replace(/u/g, '');
+  return newString;
+  
+
+}
+// console.log(remove('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'));
+
+function products(arr){
+  let newArr =[];
+  for(let i = 0; i < arr.length; i++){
+    let tempArr = arr.filter(item => item !== arr[i]);
+    let product = tempArr[0]*tempArr[1]*tempArr[2];
+    newArr.push(product);
+  
+  }
+  return newArr;
+}
+// console.log(products(arr));
+
+function stringRotation(str1,str2){
+  
+
+}
